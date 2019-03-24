20528b99382726aef872be9f778f6ace24218981
Loo Rong Jie <loorongjie@gmail.com>
2019-03-24 20:16:12 +0800

    Implement Account constraint trigger

--- sql/schema.sql
+++ sql/schema.sql
@@ -27,3 +27,57 @@ CREATE TABLE CareTaker(
   PRIMARY KEY(aid),
   FOREIGN KEY(aid) REFERENCES Account
 );
+
+/**
+ * Triggers
+ */
+
+
+/**
+ * Constraint 1: Enforce an Account cannot be both PetOwner and CareTaker.
+ */
+CREATE OR REPLACE FUNCTION not_caretaker()
+RETURNS TRIGGER AS
+$$
+DECLARE count NUMERIC;
+BEGIN
+  SELECT COUNT (*) INTO count
+  FROM CareTaker
+  WHERE NEW.aid = CareTaker.aid;
+  IF count > 0 THEN
+    RETURN NULL;
+  ELSE
+    RETURN NEW;
+  END IF;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER non_caretaker
+BEFORE INSERT OR UPDATE
+ON PetOwner
+FOR EACH ROW
+EXECUTE PROCEDURE not_caretaker();
+
+CREATE OR REPLACE FUNCTION not_petowner()
+RETURNS TRIGGER AS
+$$
+DECLARE count NUMERIC;
+BEGIN
+  SELECT COUNT (*) INTO count
+  FROM PetOwner
+  WHERE NEW.aid = PetOwner.aid;
+  IF count > 0 THEN
+    RETURN NULL;
+  ELSE
+    RETURN NEW;
+  END IF;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER non_petowner
+BEFORE INSERT OR UPDATE
+ON CareTaker
+FOR EACH ROW
+EXECUTE PROCEDURE not_petowner();
